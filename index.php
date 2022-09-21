27732dd45303a4515f10f757b6b2f563fdd6a229
kazakeviciuteviktorija <kazakeviciute.viktorija@gmail.com>
2022-09-21 19:16:41 +0300

    mano pradzia

--- index.php
+++ index.php
@@ -5,21 +5,46 @@ declare(strict_types=1);
 <!DOCTYPE html>
 <html>
 <body>
-<fieldset>
-    <legend> Ad portal </legend>
-    <form action="submit.php" method="POST">
-        <div><input type="text" name="new Add" placeholder="tasks"/>
-            <input type="submit"/>
-            <input type="date"/>
-            <input type="time"/></div>
-</fieldset>
+<fieldset <body style='background-color:lightgoldenrodyellow'>
+    <div align="center"><a href="" > <br/> <b>Ad portal </b><br/></a></div>
+    <div>
+        <a href=""> <br/> List ads </a>
+        <a href="" align="center"> Login</a>
+        <a href="" align="right"> Register</a>
+    </div>
+
+    </fieldset>
 
 </form>
-<fieldset>
-    <legend> TODOs</legend>
-    <div>
+<div>
+    <div> <b> <br/>VW golf <br/></b></div>
+    <div> <br/>great car <br/></div>
+    <div> Price: 1eur </div>
+    <div> Contact phone: 1 </div>
+    <div> Created At: 2022-06-09 15:25:17 </div>
+</div>
 
+    <div>
+        <div> <b><br/>VW golf 6 <br/></b></div>
+        <div> <br/>almost new <br/></div>
+        <div> Price: 1500eur </div>
+        <div> Contact phone: 123456 </div>
+        <div> Created At: 2022-06-09 17:15:14 </div>
     </div>
-</fieldset>
+
+    <div>
+        <div> <b> <br/>Car <br/></b></div>
+        <div> <br/>good <br/></div>
+        <div> Price: 155eur </div>
+        <div> Contact phone: 12345678 </div>
+        <div> Created At: 2022-06-13 15:27:12 </div>
+    </div>
+    <div>
+        <div> <b> <br/>Car 2<br/></b></div>
+        <div> <br/>bad <br/></div>
+        <div> Price: 123eur </div>
+        <div> Contact phone: 123456 </div>
+        <div> Created At: 2022-06-13 15:27:43 </div>
+    </div>
 </body>
 </html>
