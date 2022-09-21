a52be71e40a7361da0fa73318ca12d4af54fb46d
kazakeviciuteviktorija <kazakeviciute.viktorija@gmail.com>
2022-09-21 19:28:51 +0300

    mano pradzia

--- index.php
+++ index.php
@@ -1,50 +1,49 @@
 <?php
+
 declare(strict_types=1);
-//require_once './functions.php';
+
 ?>
+
 <!DOCTYPE html>
 <html>
-<body>
-<fieldset <body style='background-color:lightgoldenrodyellow'>
-    <div align="center"><a href="" > <br/> <b>Ad portal </b><br/></a></div>
-    <div>
-        <a href=""> <br/> List ads </a>
-        <a href="" align="center"> Login</a>
-        <a href="" align="right"> Register</a>
+<body style="max-width: 1200px; margin: auto">
+<nav style="background: rgba(152,147,63,0.35); padding: 25px">
+    <h2 style="display: flex; justify-content: space-around">
+        <a href="/">Best ads website</a>
+    </h2>
+    <div style="display: flex; justify-content: space-around">
+        <div>
+            <a href="/">List ads</a>
+        </div>
+        <div>
+            <a href="/">Login</a>
+        </div>
+        <div>
+            <a href="/">Register</a>
+        </div>
     </div>
-
-    </fieldset>
-
-</form>
+</nav>
 <div>
-    <div> <b> <br/>VW golf <br/></b></div>
-    <div> <br/>great car <br/></div>
-    <div> Price: 1eur </div>
-    <div> Contact phone: 1 </div>
-    <div> Created At: 2022-06-09 15:25:17 </div>
+    <p>
+    <h3><?php echo 'test'; ?></h3>
+    <ul>
+        <li>
+            <?= 'test without echo' ?>
+        </li>
+        <li>
+            Price: 1eur
+        </li>
+        <li>
+            Kaunas
+        </li>
+        <li>
+            Contact phone: +37013245678
+        </li>
+        <li>
+            Created at: 2022-06-01 01:15
+        </li>
+    </ul>
+    </p>
 </div>
-
-    <div>
-        <div> <b><br/>VW golf 6 <br/></b></div>
-        <div> <br/>almost new <br/></div>
-        <div> Price: 1500eur </div>
-        <div> Contact phone: 123456 </div>
-        <div> Created At: 2022-06-09 17:15:14 </div>
-    </div>
-
-    <div>
-        <div> <b> <br/>Car <br/></b></div>
-        <div> <br/>good <br/></div>
-        <div> Price: 155eur </div>
-        <div> Contact phone: 12345678 </div>
-        <div> Created At: 2022-06-13 15:27:12 </div>
-    </div>
-    <div>
-        <div> <b> <br/>Car 2<br/></b></div>
-        <div> <br/>bad <br/></div>
-        <div> Price: 123eur </div>
-        <div> Contact phone: 123456 </div>
-        <div> Created At: 2022-06-13 15:27:43 </div>
-    </div>
 </body>
 </html>
