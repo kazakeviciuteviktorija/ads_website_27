48a186e1870f3bbb0b5e913a1719fd1ec6fd2a92
kazakeviciuteviktorija <kazakeviciute.viktorija@gmail.com>
2022-09-21 17:38:46 +0300

    siaip

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+//require_once './functions.php';
+?>
+<!DOCTYPE html>
+<html>
+<body>
+<fieldset>
+    <legend> Ad portal </legend>
+    <form action="submit.php" method="POST">
+        <div><input type="text" name="new Add" placeholder="tasks"/>
+            <input type="submit"/>
+            <input type="date"/>
+            <input type="time"/></div>
+</fieldset>
+
+</form>
+<fieldset>
+    <legend> TODOs</legend>
+    <div>
+
+    </div>
+</fieldset>
+</body>
+</html>
